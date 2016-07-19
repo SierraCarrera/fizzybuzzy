c7830ed93a04a0f3dff02c9cfc52b43274ddb261
Joel S Sierra <jsierra@student.cscc.edu>
2016-07-19 18:19:01 -0400

    Console skeleton

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace FizzBuzzUI {
+    internal class FizzBuzz {
+        private int max;
+
+        public FizzBuzz(int max) {
+            this.max = max;
+        }
+
+        public IEnumerable<string> Results { get; internal set; }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzUI {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter a number up to 100");
+            var max = Convert.ToInt32(Console.ReadLine());
+
+            var fizzBuzz = new FizzBuzz(max);
+
+            foreach(var item in fizzBuzz.Results) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
